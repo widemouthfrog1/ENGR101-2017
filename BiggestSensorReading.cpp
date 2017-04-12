91f9587434c9262c08ca273b6f997bb910d8a940
widemouthfrog1 <widemouthfrog1@gmail.com>
2017-04-13 10:52:52 +1200

    Create BiggestSensorReading.cpp

---
+++ BiggestSensorReading.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+int main(){
+  init();
+  int adc_reading;
+  int adc_biggestReading = 0;
+  for (int i = 0; i<10; i=i+1){
+    adc_reading = read_analog(0);
+    if (adc_reading > adc_biggestReading){
+      adc_biggestReading = adc_reading;
+    }
+    sleep1(0,500000);
+    sleep1(0,500000);
+  }
+  returnprintf("%d\n",adc_biggestReading);
+  sleep1(0,500000);
+return 0;
+}
